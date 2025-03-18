86e94d3a85b1322809837e3ad3412a7240c8abcf
Olena Chepurna <lchep@ukr.net>
2025-03-18 15:11:43 +0200

    edit task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,15 @@
 
 const inputs = document.querySelectorAll('form input');
 
-inputs.forEach(input => {
+inputs.forEach((input) => {
   const label = document.createElement('label');
+
   label.classList.add('field-label');
   label.setAttribute('for', input.id);
   label.textContent = input.name;
   input.parentNode.insertBefore(label, input);
 
   const placeholder = input.name.toUpperCase();
+
   input.setAttribute('placeholder', placeholder);
 });
